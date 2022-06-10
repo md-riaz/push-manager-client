b799ecf69fb2018e9abec481a494116e3baf2dd3
mdriaz <riazmd582@gmail.com>
2022-06-10 18:00:10 +0600

    testing service worker

--- static/push-manager-worker.js
+++ static/push-manager-worker.js
@@ -3,8 +3,20 @@ console.log('Service Worker Loaded...');
 self.addEventListener('push', (e) => {
 	const data = e.data.json();
 	console.log('Push Recieved...');
-	self.registration.showNotification(data.title, {
+
+	const promiseChain = self.registration.showNotification(data.title, {
 		body: data.body,
 		icon: data.icon
 	});
+
+	e.waitUntil(promiseChain);
+});
+
+self.addEventListener('notificationclick', function (event) {
+	const clickedNotification = event.notification;
+	clickedNotification.close();
+	console.log(event);
+	// Do something as the result of the notification click
+	// const promiseChain = doSomething();
+	// event.waitUntil(promiseChain);
 });
